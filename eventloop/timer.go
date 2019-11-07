8e3b38fd32d7e829cf10a41ae8b3e59c2ca6cf5b
iakud <kaikai87111@gmail.com>
2019-11-07 18:37:56 +0800

    timer

--- eventloop/timer.go
+++ eventloop/timer.go
@@ -5,13 +5,23 @@ import (
 )
 
 type Timer struct {
-	*time.Timer
+	t *time.Timer
 }
 
 func newTimer(loop *EventLoop, d time.Duration, f func()) *Timer {
 	t := time.AfterFunc(d, func() {
 		loop.RunInLoop(f)
 	})
-	timer := &Timer{t}
+	timer := &Timer{
+		t: t,
+	}
 	return timer
+}
+
+func (this *Timer) Stop() bool {
+	return this.t.Stop()
+}
+
+func (this *Timer) Reset(d time.Duration) bool {
+	return this.t.Reset(d)
 }
