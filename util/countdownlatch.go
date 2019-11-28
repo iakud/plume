bb87b9a4339018818c0e6e33a599344b67e0b342
iakud <kaikai87111@gmail.com>
2019-11-28 12:24:15 +0800

    add countdownlatch

---
+++ util/countdownlatch.go
@@ -0,0 +1,36 @@
+package util
+
+import (
+	"sync"
+)
+
+type CountDownLatch struct {
+	mutex sync.Mutex
+	cond  *sync.Cond
+	count int
+}
+
+func NewCountDownLatch(count int) *CountDownLatch {
+	countDownLatch := &CountDownLatch{
+		count: count,
+	}
+	countDownLatch.cond = sync.NewCond(&countDownLatch.mutex)
+	return countDownLatch
+}
+
+func (this *CountDownLatch) Wait() {
+	this.mutex.Lock()
+	defer this.mutex.Unlock()
+	for this.count > 0 {
+		this.cond.Wait()
+	}
+}
+
+func (this *CountDownLatch) CountDown() {
+	this.mutex.Lock()
+	defer this.mutex.Unlock()
+	this.count--
+	if this.count == 0 {
+		this.cond.Broadcast()
+	}
+}
