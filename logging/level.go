7fab239ccd560d731a4288bfc41864e577515655
iakud <kaikai87111@gmail.com>
2020-05-09 11:14:57 +0800

    log level

---
+++ logging/level.go
@@ -0,0 +1,26 @@
+package logging
+
+type Level int32
+
+const (
+	TraceLevel Level = iota
+	DebugLevel
+	InfoLevel
+	WarnLevel
+	ErrorLevel
+	FatalLevel
+	numLevel = 6
+)
+
+var levelName = [numLevel]string{
+	TraceLevel: "TRACE ",
+	DebugLevel: "DEBUG ",
+	InfoLevel:  "INFO  ",
+	WarnLevel:  "WARN  ",
+	ErrorLevel: "ERROR ",
+	FatalLevel: "FATAL ",
+}
+
+func (this Level) String() string {
+	return levelName[this]
+}
