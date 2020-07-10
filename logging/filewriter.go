759ad15f2ac32a0aec37ca2ae26853d0aaa9f290
iakud <kaikai87111@gmail.com>
2020-07-10 15:40:18 +0800

    delete old rolls

--- logging/filewriter.go
+++ logging/filewriter.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
+	"strings"
 	"sync"
 	"time"
 )
@@ -27,19 +29,28 @@ type FileWriter struct {
 	buffer *bufio.Writer
 	closed bool
 
+	maxRolls   int
+	history    []string
 	filePeriod time.Time
 }
 
-func NewFileWriter(path string, period time.Duration) *FileWriter {
+// maxRools: if <= 0, unlimited
+func NewFileWriter(path string, period time.Duration, maxRolls int) *FileWriter {
 	ctx, cancel := context.WithCancel(context.Background())
 	dir, name := filepath.Split(path)
 	fw := &FileWriter{
-		dir:    dir,
+		dir:    filepath.Dir(dir),
 		name:   name,
 		period: period,
 		cancel: cancel,
-	}
 
+		maxRolls: maxRolls,
+	}
+	if maxRolls > 0 {
+		if history, err := fw.historyRolls(); err == nil {
+			fw.history = history
+		}
+	}
 	go fw.flushPeriodically(ctx)
 	return fw
 }
@@ -126,6 +137,12 @@ func (fw *FileWriter) rollFile(t time.Time) error {
 	}
 	fw.file = file
 	fw.buffer = bufio.NewWriterSize(file, kBufferSize)
+	if fw.maxRolls > 0 {
+		if stat, err := file.Stat(); err == nil {
+			fw.history = append(fw.history, stat.Name())
+			fw.removeOldRolls()
+		}
+	}
 	return nil
 }
 
@@ -144,4 +161,40 @@ func (fw *FileWriter) createFile(t time.Time) (*os.File, error) {
 		os.Link(name, symlink)
 	}
 	return file, nil
+}
+
+func (fw *FileWriter) historyRolls() ([]string, error) {
+	f, err := os.Open(fw.dir)
+	if err != nil {
+		return nil, err
+	}
+	files, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		return nil, err
+	}
+	var history []string
+	for _, file := range files {
+		// regular files
+		if file.Mode()&os.ModeType != 0 {
+			continue
+		}
+		// filter
+		if strings.HasPrefix(file.Name(), fw.name) {
+			history = append(history, file.Name())
+		}
+	}
+	sort.Sort(sort.StringSlice(history)) // sort by string
+	return history, nil
+}
+
+func (fw *FileWriter) removeOldRolls() {
+	if nRolls := len(fw.history); nRolls > fw.maxRolls {
+		removeRools := nRolls - fw.maxRolls
+		for _, name := range fw.history[:removeRools] {
+			filename := filepath.Join(fw.dir, name)
+			os.Remove(filename) // ignore err
+		}
+		fw.history = fw.history[removeRools:]
+	}
 }
--- logging/filewriter_test.go
+++ logging/filewriter_test.go
@@ -8,17 +8,28 @@ import (
 
 func TestFileWriter(t *testing.T) {
 
-	fw := NewFileWriter(t.Name()+".log", time.Hour)
+	fw := NewFileWriter(t.Name()+".log", time.Hour, 0)
 	defer fw.Close()
 	s := fmt.Sprintf("open file: %s", time.Now())
 	fw.Write([]byte(s))
 }
 
 func TestFileWriterFlush(t *testing.T) {
-	fw := NewFileWriter(t.Name()+".log", time.Hour)
+	fw := NewFileWriter(t.Name()+".log", time.Hour, 0)
 	s := fmt.Sprintf("open file: %s\n", time.Now())
 	fw.Write([]byte(s))
 	fw.Write([]byte("flush\n"))
 	fw.Flush()
 	fw.Write([]byte("after flush\n"))
+}
+
+func TestFileWriterRolls(t *testing.T) {
+	maxRolls := 2
+	fw := NewFileWriter(t.Name()+".log", time.Second, maxRolls)
+	defer fw.Close()
+	fw.Write([]byte("test 1"))
+	time.Sleep(time.Second)
+	fw.Write([]byte("test 2"))
+	time.Sleep(time.Second)
+	fw.Write([]byte("test 3"))
 }
